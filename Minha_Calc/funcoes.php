5da528c75e684d2c7e6c0d4dd254dbcafde32b27
analyceferreira <37940243+analyceferreira@users.noreply.github.com>
2018-03-30 15:17:11 -0400

    Assim também não deu certo ):

---
+++ Minha_Calc/funcoes.php
@@ -0,0 +1,16 @@
+<?php
+	function soma($val1,$val2,$opcao){
+		echo $val1 + $val2;
+	}
+	function subtracao($val1,$val2,$opcao){
+		echo $val1 - $val2;
+	}
+	
+	function divisao($val1,$val2,$opcao){
+		echo $val1 / $val2;
+	}
+	
+	function multiplicacao($val1,$val2,$opcao){
+		echo $val1 * $val2;
+	}
+ ?>
